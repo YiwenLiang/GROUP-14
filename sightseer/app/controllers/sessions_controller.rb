451c4e7d9556bd075f6fe7b188fd296cdee4f43d
Sam Cribb <samuel.cribb@gmail.com>
2016-02-20 10:23:04 +0000

    Added username/firstname/lastname/email

--- sightseer/app/controllers/sessions_controller.rb
+++ sightseer/app/controllers/sessions_controller.rb
@@ -5,10 +5,11 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(name: params[:name])
+    user = User.find_by(username: params[:username])
     if user and user.authenticate(params[:password])
         session[:user_id] = user.id
-        session[:user_name] = user.name
+        session[:user_username] = user.username
+
         redirect_to users_url, alert: "Sucessfully Logged in"
     else
         redirect_to login_url, alert:"Wrong Username or Password!"
--- sightseer/db/schema.rb
+++ sightseer/db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160218025749) do
+ActiveRecord::Schema.define(version: 20160220095643) do
 
   create_table "users", force: :cascade do |t|
-    t.string   "name"
-    t.string   "password_digest"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.string "username"
+    t.string "firstname"
+    t.string "lastname"
+    t.string "email"
+    t.string "password_digest"
   end
 
 end
