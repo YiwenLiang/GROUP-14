dc23c6b81383e9a06ff11a1fcea25b4f39e59e98
Sam Cribb <samuel.cribb@gmail.com>
2016-02-22 04:53:27 +0000

    Login Complete

--- sightseer/app/controllers/admin_controller.rb
+++ sightseer/app/controllers/admin_controller.rb
@@ -24,7 +24,7 @@ class AdminController < ApplicationController
   def destroy
     @user.destroy
     respond_to do |format|
-      format.html { redirect_to admin_url, notice: 'User was successfully destroyed.' }
+      format.html { redirect_to admin_index_url, notice: 'User was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
--- sightseer/app/controllers/users_controller.rb
+++ sightseer/app/controllers/users_controller.rb
@@ -1,29 +1,25 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
   skip_before_action :authorize, only: [:new, :create, :index] 
-  # GET /users
-  # GET /users.json
+
   def index
     @users = User.all
   end
 
-  # GET /users/1
-  # GET /users/1.json
+
   def show
   end
 
-  # GET /users/new
+
   def new
     @user = User.new
   end
 
-  # GET /users/1/edit
+
   def edit
    @user = User.find(params[:id])
   end
 
-  # POST /users
-  # POST /users.json
   def create
     @user = User.new(user_params)
     @user.admin_type ||= false
@@ -38,8 +34,7 @@ class UsersController < ApplicationController
     end
   end
 
-  # PATCH/PUT /users/1
-  # PATCH/PUT /users/1.json
+
   def update
     respond_to do |format|
       if @user.update(user_params)
@@ -64,25 +59,23 @@ class UsersController < ApplicationController
 
 
 
-  # DELETE /users/1
-  # DELETE /users/1.json
   def destroy
     @user.destroy
     respond_to do |format|
-      format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
+      format.html { redirect_to admin_url, notice: 'User was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
 
 
   private
-    # Use callbacks to share common setup or constraints between actions.
+
     def set_user
       @user = User.find(params[:id])
     end
   
-    # Never trust parameters from the scary internet, only allow the white list through.
+
     def user_params
-      params.require(:user).permit(:username, :firstname, :lastname, :email, :password, :password_confirmation,  :admin_type)
+      params.require(:user).permit(:username, :firstname, :lastname, :email, :password, :password_confirmation, :admin_type)
     end
 end
