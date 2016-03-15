22f6c0798c385abd1ca822a0be6099da3daa0965
Sam Cribb <samuel.cribb@gmail.com>
2016-03-15 02:02:28 +0000

    fix master

---
+++ sightseer/db/seeds.rb
@@ -0,0 +1,5 @@
+User.create(username: 'admin', password:'123' , admin_type: 'true')
+User.create(username: 'Group', firstname:'The', lastname: 'Group', email: 'Group14@gmail.com', password:'14' , admin_type: 'true')
+User.create(username: 'CoolDude123', firstname:'Billy', lastname: 'Joe', email: 'Billy_Joe@gmail.com', password:'123' , admin_type: 'false')
+User.create(username: 'NiceGuy', firstname:'Bobby', lastname: 'Jean', email: 'Nice_Jeans@gmail.com', password:'123' , admin_type: 'false')
+User.create(username: 'Random', firstname:'Guy', lastname: 'Fieri', email: 'Spikey.hair@gmail.com', password:'123' , admin_type: 'false')
