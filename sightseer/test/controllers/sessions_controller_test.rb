1d32073fccb301a68a24b1b6be4272b6b396648a
crossleaf <danielkao1995@gmail.com>
2016-03-15 02:14:27 +0000

    Tests

--- sightseer/test/controllers/sessions_controller_test.rb
+++ sightseer/test/controllers/sessions_controller_test.rb
@@ -6,14 +6,4 @@ class SessionsControllerTest < ActionController::TestCase
     assert_response :success
   end
 
-  test "should get create" do
-    get :create
-    assert_response :success
-  end
-
-  test "should get destroy" do
-    get :destroy
-    assert_response :success
-  end
-
 end
