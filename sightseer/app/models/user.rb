f6cd3cf31ecb0daa076be53362c27c6ca5863eaa
crossleaf <danielkao1995@gmail.com>
2016-03-15 01:23:30 +0000

    tests

--- sightseer/app/models/user.rb
+++ sightseer/app/models/user.rb
@@ -1,7 +1,9 @@
 class User < ActiveRecord::Base
   has_secure_password
+    validates :firstname, presence: true
+    validates :lastname, presence: true
+    validates :username, presence: true, uniqueness: true
+    validates :email, presence: true
+    validates :password_confirmation, presence: true
 
-
-
-  
 end
--- sightseer/test/models/user_test.rb
+++ sightseer/test/models/user_test.rb
@@ -4,4 +4,49 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test "Missing first name" do
+    user = User.new
+    assert !user.save
+    assert !user.errors[:firstname].empty?
+  end
+  
+  test "Missing username" do
+    user = User.new
+    assert !user.save
+    assert !user.errors[:username].empty?
+  end
+  
+   test "Username unique" do
+    user1 = User.new(username: '123')
+    assert !user1.valid?
+    user2=User.new(username:'123')
+    assert !user2.valid?
+    assert_not_nil user2.errors[:username]
+  end
+  
+  test "Missing last name" do
+    user = User.new
+    assert !user.save
+    assert !user.errors[:lastname].empty?
+  end
+  
+  test "Missing email" do
+    user = User.new
+    assert !user.save
+    assert !user.errors[:email].empty?
+  end
+  
+  test "Missing password" do
+    user = User.new
+    assert !user.save
+    assert !user.errors[:password].empty?
+  end
+  
+  test "Missing password confirmation" do
+    user = User.new
+    assert !user.save
+    assert !user.errors[:password_confirmation].empty?
+  end
+
+  
 end
