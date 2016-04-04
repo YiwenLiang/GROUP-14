6352c3bc40ef52a9e17a4f11a9b14c6ee2ac8c7b
ktp693 <chang06151995@hotmail.com>
2016-04-04 03:36:08 +0000

    adding checker and freelancer

--- sightseer/app/models/user.rb
+++ sightseer/app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
     validates :firstname, presence: true
     validates :lastname, presence: true
     validates :username, presence: true, uniqueness: true
-    validates :email, email_format: { message: "Sorry! Please input real email!" }
+    validates :email, email_format: { message: "Wrong! Please input real email!" }
     validates_length_of :password, :in => 6..20, :on => :create
     validates :password_confirmation, presence: true
     
