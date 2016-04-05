d09d39bc3a8879d89e59768087d1962ebbc59f02
crossleaf <danielkao1995@gmail.com>
2016-04-05 04:28:08 +0000

    adding more tests

--- sightseer/test/controllers/users_controller_test.rb
+++ sightseer/test/controllers/users_controller_test.rb
@@ -9,4 +9,16 @@ class UsersControllerTest < ActionController::TestCase
     get :new
     assert_response :success
   end
+  
+  test "Create User" do
+    user=User.new
+    user.username="123"
+    user.firstname="hello"
+    user.lastname="world"
+    user.email="helloworld@sfu.ca"
+    user.password="123456"
+    user.password_confirmation="123456"
+    user.admin_type=0
+    assert user.valid?
+  end
 end
